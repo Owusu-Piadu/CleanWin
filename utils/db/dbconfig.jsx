3b1cb312a9feea0b0328469b5e3c1d9593bcdaf5
Tarirohope <Priscilakapuyanyika974@gmail.com>
2024-09-13 12:38:14 -0600

    db config

--- utils/db/dbconfig.jsx
+++ utils/db/dbconfig.jsx
@@ -1,7 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-const sql = neon(
-   "postgresql://Ryde_owner:f0b4yausBltm@ep-gentle-snow-a5w1vgu4.us-east-2.aws.neon.tech/%20%20cleanwin?sslmode=require"
-);
+const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql, { schema });
